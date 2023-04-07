a8c96810689a09594ee6e448f89012cfa68fe53d
Thomas Chevalier <git@tchevalier.fr>
2023-04-07 16:53:52 +0200

    Add gcc installation script

---
+++ zephyr-docker/workspace/install-gcc.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# apply changes to the libs 
+export WORKDIR_PATH=/workdir
+export ZEPHYR_SDK_PATH=/opt/toolchains/zephyr-sdk-0.15.1
+
+cd /workdir/zephyr-patch
+echo "\n\n" | sudo /workdir/zephyr-patch/copy-zephyr-files.py > /dev/null
+cd /workdir
+
+cd /opt/toolchains/zephyr-sdk-0.15.1/riscv64-zephyr-elf/bin/
+
+cat << EOF > riscv64-zephyr-elf-gcc.wrapper
+#!/bin/sh
+exec /opt/toolchains/zephyr-sdk-0.15.1/riscv64-zephyr-elf/bin/riscv64-zephyr-elf-gcc.real -fplugin=/workdir/inst_plugin_cfi.so "\$@"
+EOF
+sudo chmod +x riscv64-zephyr-elf-gcc.wrapper
+
+sudo mv riscv64-zephyr-elf-gcc riscv64-zephyr-elf-gcc.real
+sudo ln -s riscv64-zephyr-elf-gcc.wrapper riscv64-zephyr-elf-gcc
+sudo chmod +x riscv64-zephyr-elf-gcc
